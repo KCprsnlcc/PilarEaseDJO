4b98236142075b805540e8377feca7cd4ca1de39
KCprsnlcc <kcpersonalacc@gmail.com>
2024-09-08 23:40:17 +0800

    Fixed the Verification email interface

--- main/static/js/email.js
+++ main/static/js/email.js
@@ -20,3 +20,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   setInterval(displayRandomTip, 5000); // Update tip every 5 seconds
 });
+
+document.querySelectorAll(".curved-line path").forEach(function (path) {
+  var controlPointX1 = Math.random() * 50;
+  var controlPointY1 = Math.random() * 50;
+  var controlPointX2 = 100 - Math.random() * 50;
+  var controlPointY2 = Math.random() * 50;
+  var endPointX = Math.random() * 100;
+  var endPointY = Math.random() * 100;
+  var d = `M0,0 C${controlPointX1},${controlPointY1} ${controlPointX2},${controlPointY2} ${endPointX},${endPointY}`;
+  path.setAttribute("d", d);
+});
