a1f21bd13895f03e498852173f9df9322add76b9
KCprsnlcc <kcpersonalacc@gmail.com>
2024-10-13 00:17:21 +0800

    Altered PilarEaseDJO Settings

--- PilarEaseDJO/urls.py
+++ PilarEaseDJO/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # PilarEaseDJO/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', include('admin_tools.urls')),
+    path('itrc/', include('itrc_tools.urls')),
     path('admin/', RedirectView.as_view(url='/admin/login/', permanent=True)),
     path('__debug__/', include('debug_toolbar.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
